3fdc76e6768a1410d60dd0079c1ab6a3e01218c8
cj <sigamateusz@gmail.com>
2017-10-20 22:34:09 +0200

    Add config for Chuck Norris Bean

---
+++ src/main/java/guru/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,13 @@
+package guru.springframework.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
